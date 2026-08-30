04e270a20ac32f588cd91ae39ef1b606946606b9
cifuuscobam <cifarelli.martina02@gmail.com>
2026-08-31 00:04:59 +0200

    menu tasto chiudi

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,20 @@ public class MainMenu : MonoBehaviour
     public void Gioca()
     {
         // Carica la scena successiva nella coda dei Build Settings
-        // In alternativa puoi usare: SceneManager.LoadScene("NomeDellaTuaScena");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Questa è la nuova funzione da collegare al tasto Esci
+    public void ChiudiGioco()
+    {
+        Debug.Log("Il gioco si sta chiudendo...");
+
+        // Chiude l'applicazione quando il gioco è esportato (Build)
+        Application.Quit();
+
+        // Ferma il Play Mode nell'Editor di Unity
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
